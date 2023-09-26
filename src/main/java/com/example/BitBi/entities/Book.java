fb4a6614fecfc54687d642646837125c23bc9b96
Riccardo <rcr.casale@gmail.com>
2023-09-26 15:48:29 +0200

    i added book elements

---
+++ src/main/java/com/example/BitBi/entities/Book.java
@@ -0,0 +1,16 @@
+package com.example.BitBi.entities;
+
+public class Book {
+
+/*id, titolo, isbn, autore, npagine, categoria, genere, illustrazioni (boolean), autore illustrazioni (opzionale), commento
+, lingua originale*/
+
+    private int id;
+    private String titolo;
+    private String isbn;
+    private String autore;
+    private int nPagine;
+
+
+
+}
