6b63b4373bf5fbd1dc7768c03c8cb75ded2a2329
Riccardo <rcr.casale@gmail.com>
2023-09-26 12:23:39 +0200

    let's try again

--- src/main/java/com/example/BitBi/BitBiApplication.java
+++ src/main/java/com/example/BitBi/BitBiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BitBiApplication {
-
+//NO
 	public static void main(String[] args) {
 		SpringApplication.run(BitBiApplication.class, args);
 	}
