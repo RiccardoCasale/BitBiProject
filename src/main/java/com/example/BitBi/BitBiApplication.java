bf4aeec119bd68d2b2998dd4af6cc438194131d0
Riccardo <rcr.casale@gmail.com>
2023-09-26 11:31:44 +0200

    let's start now

---
+++ src/main/java/com/example/BitBi/BitBiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BitBi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitBiApplication.class, args);
+	}
+
+}
