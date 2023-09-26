779ae48ec7a021ef2f11d167d155bb3406606580
Riccardo <rcr.casale@gmail.com>
2023-09-26 11:31:44 +0200

    let's start now

---
+++ src/test/java/com/example/BitBi/BitBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BitBi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
