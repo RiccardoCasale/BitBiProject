b22f3428b93fadcc344701756ee93e249ad97992
Riccardo <rcr.casale@gmail.com>
2023-09-28 11:03:09 +0200

    book class finished

--- src/main/java/com/example/BitBi/dto/BookDto.java
+++ src/main/java/com/example/BitBi/dto/BookDto.java
@@ -1,5 +1,16 @@
 package com.example.BitBi.dto;
 
+//DATA TRANSFER OBJECT. è un design pattern. Lo usiamo per trasferire dati tra i diversi componenti dell'applicazione
+@Data
 public class BookDto {
+
+    private int id;
+    private String titolo;
+    private String autore;
+    private String categoria;
+    private String genere;
+    private int nPagine;
+    private String autoreIllustrazioni;
+    private String lingua;
 
 }
